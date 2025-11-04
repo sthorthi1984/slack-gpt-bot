0a6c266a3a5f096298d0c876f213ff0bc3cfaa2d
sthorthi1984 <sthorthi@avertra.com>
2025-11-04 18:21:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from slack_sdk.errors import SlackApiError
 from slack_sdk.signature import SignatureVerifier
 from difflib import get_close_matches
 from dotenv import load_dotenv
-from docx import Document
+from doc import Document
 import openai
 
 # Load environment variables
